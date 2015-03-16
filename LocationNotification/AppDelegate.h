e5e29d281c8dc72dd01772995d4a35e3ce26cc24
GeorgeHan <hanyangkid@163.com>
2015-03-16 09:43:14 +0800

    Initial Commit

---
+++ LocationNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationNotification
+//
+//  Created by iwind on 15/3/16.
+//  Copyright (c) 2015年 MUGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationNotification
+//
+//  Created by iwind on 15/3/16.
+//  Copyright (c) 2015年 MUGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
